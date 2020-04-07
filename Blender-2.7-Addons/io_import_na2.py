68d67efac0e6e026240d8c6cdf2b5f46ad370488
deliverator23 <iain.millar@gmail.com>
2020-04-07 09:56:41 +0100

    Remove irrelevant and incorrect comment!

--- Blender-2.7-Addons/io_import_na2.py
+++ Blender-2.7-Addons/io_import_na2.py
@@ -194,7 +194,6 @@ def import_na2(path):
 				poseBone = armOb.pose.bones[boneName]
 				if poseBone != None:
 
-					# Correct position but wrong rotations
 					animMatrix = Matrix([[x[2][0],x[0][0],x[1][0],x[3][0]],
 									 [x[2][1],x[0][1],x[1][1],x[3][1]],
 									 [x[2][2],x[0][2],x[1][2],x[3][2]],
